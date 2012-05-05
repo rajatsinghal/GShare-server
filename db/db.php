0a7fd48fb9f8bf371135627ac99fe5aea249dd8e
Rajat Singhal <rajat@octathorpeweb.com>
2012-05-06 04:08:44 +0530

    first_commit

---
+++ db/db.php
@@ -0,0 +1,40 @@
+<?php
+class db{
+    private $config = array(
+        "server" => "localhost",
+        "username" => "root",
+        "password" => "root",
+        "database" => "gshare");
+    private $connection;
+    private $db;
+    
+    private function connect() {
+        $server = $this->config["server"];
+        $username = $this->config["username"];
+        $password = $this->config["password"];
+        $this->connection = mysql_connect($server, $username, $password);
+        $this->db = mysql_select_db($this->config["database"]);
+    }
+    
+    public function __construct() {
+        $this->connect();        
+    }
+    
+    public static function lib() {
+        return new db;
+    }
+    
+    public function execute($query) {
+        return mysql_query($query);
+    }
+    
+    public function findAll($query) {
+        $mysql_result = $this->execute($query);
+        $result_data = array();
+        while($row = mysql_fetch_array($mysql_result)) {
+            array_push($result_data, $row);
+        }
+        return mysql_result;
+    }
+}
+?>
